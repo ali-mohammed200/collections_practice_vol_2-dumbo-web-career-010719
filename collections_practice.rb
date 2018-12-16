484b0fd48984a818bc1ca86994ae0f6ce3165304
Mohammad Ali <ali.mohammed200@gmail.com>
2018-12-16 04:33:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
 # your code goes here
+def begins_with_r(arr)
+  arr.all? { |e| e[0] == "r" }
+end
+def contain_a(arr)
+  arr.select{ |elem| elem.include?("a")}
+end
+def first_wa(arr)
+  return arr.find {|word|
+    if word.class == String
+      word.include?("wa")
+    end }
+end
+def remove_non_strings(arr)
+  arr.select { |e| e.class == String }
+end
+def count_elements(arr)
+   #{:count => arr.count(t), t}}
+  #arr.uniq.map{|t| [t,arr.count(t)]}.to_h
+  #while i < uniqArr.length
+  #arr.each{|obj|
+  #  if uniqArr[i] == obj
+  #    count += 1
+  #    uniqArr[:count] = count
+  #  end
+  #}
+  #i += 1
+  count = 0
+  uniqArr = arr.uniq
+  current = ""
+  uniqArr.map.with_index {|obj, i|
+    if obj ==
+      count += 1
+      obj[:count] = count
+    end
+  }
+end
